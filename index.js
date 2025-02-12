47606cc57c8f00d800b60f09de05eecd3ee94754
Sumedh <sumedhzodape143@gmail.com>
2025-02-12 20:07:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+let server = http.createServer((req, res)=>{
+
+    // console.log(req.method)
+    // console.log(req.url)
+
+    const parsedURL = url.parse(req.url, true);
+
+    // console.log(parsedURL)
+    // console.log(parsedURL.query)
+
+
+
+    if(req.method === "GET" && parsedURL.path ==="/getusers"){
+
+
+        // to get all the users
+
+        const userData = JSON.parse(fs.readFileSync('./users.json', 'utf-8'))
+
+        res.write(JSON.stringify({
+            message:"User fetched",
+            success: true,
+            result: userData
+        }))
+        res.end()
+    }
+    else if(req.method === "GET" && parsedURL.pathname ==="/getusers" 
+        && parsedURL.query.id !==null){
+
+            const userData = JSON.parse(fs.readFileSync('./users.json', 'utf-8'))
+
+            let singleUser = userData.find((ele, index)=>{
+                return Number(ele.id) === Number(parsedURL.query.id)
+            })
+
+
+            if(singleUser){
+                res.write(JSON.stringify(
+                    {
+                        message:"Single user fetched",
+                        success: true,
+                        result: singleUser
+                    }
+                ))
+                res.end()
+            }else{
+                res.write(JSON.stringify({
+                    message: "USER NOT FOUND",
+                    success:false
+                }))
+                res.end()
+            }
+
+    }
+    else if(req.method === "DELETE" && parsedURL.pathname === "/deleteuser" 
+        && parsedURL.query.id !== null
+    ){
+        const id = parsedURL.query.id; // 2
+        let userData = JSON.parse(fs.readFileSync('./users.json', 'utf-8')) // [{id:2}]
+
+
+        // const userIndex = userData.findIndex((ele, index)=>Number(ele.id) === Number(id))
+        const userIndex = userData.findIndex((ele, index)=>{
+            return Number(ele.id) === Number(id)
+        })
+
+        if(userIndex !== -1){
+
+            userData.splice(userIndex, 1)
+
+            fs.writeFileSync('./users.json', JSON.stringify(userData))
+
+            res.write(JSON.stringify({
+                message:"User Deleted Successfully.",
+                success:true
+            }))
+            res.end()
+            
+        }else{
+            res.write(JSON.stringify({
+                message:"Invalid User ID",
+                success: false
+            }))
+            res.end()
+        }
+
+    }
+    else{
+        res.write(JSON.stringify({
+            message:"Server Error",
+            success: false
+        }));
+        res.end()
+    }
+
+
+   
+
+
+
+})
+
+server.listen(8000, ()=>{
+    console.log("SERVER IS RUNNING...")
+})
